40fc3e13130125c2ae13b74ce3a166fd27b874e2
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-04 14:33:15 +0100

    add logo and navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,11 +4,28 @@ export default function Header(){
     return(
         <header>
             <div className="container">
-                <section>
-                    <img src={Logo} />
+                <section className="left">
+                    <img src={Logo} className="logo" />
                 </section>
-                <section>
-                    prova
+                <section className="right">
+                    <ul>
+                        <nav className="navbar navbar-expand-sm navbar-light">
+                                    <div className="navbar-nav">
+                                        <a className="nav-link active" aria-current="page" href="#">CHARACTERS</a>
+                                        <a className="nav-link active" aria-current="page" href="#">COMICS</a>
+                                        <a className="nav-link active" aria-current="page" href="#">MOVIES</a>
+                                        <a className="nav-link active" aria-current="page" href="#">TV</a>
+                                        <a className="nav-link active" aria-current="page" href="#">GAMES</a>
+                                        <a className="nav-link active" aria-current="page" href="#">COLLECTIBLES</a>
+                                        <a className="nav-link active" aria-current="page" href="#">VIDEOS</a>
+                                        <a className="nav-link active" aria-current="page" href="#">FANS</a>
+                                        <a className="nav-link active" aria-current="page" href="#">NEWS</a>
+                                        <a className="nav-link active" aria-current="page" href="#">SHOP</a>
+                                        
+                                    </div>
+                            
+                        </nav>
+                    </ul>
                 </section>
             </div>
         </header>
