eb86e8e8c8bacf0f019467a83d9b3b5911af90f0
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-04 15:40:56 +0100

    add images and description in the blue section

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,3 +1,9 @@
+import Figurina from "/img/buy-comics-digital-comics.png?url"
+import Maglietta from "/img/buy-comics-merchandise.png?url"
+import Carte from "/img/buy-comics-subscriptions.png?url"
+import Gps from "/img/buy-comics-shop-locator.png?url"
+import Batteria from "/img/buy-dc-power-visa.svg?url"
+
 export default function Main() {
     return (
         <main>
@@ -10,7 +16,11 @@ export default function Main() {
           </div>
           <div className="bg-blue">
            <section className="bottom container">
-            <p>prova</p>
+            <a className="nav-link active" aria-current="page" href="#"> <img className="fotoPiccole" src={Figurina} /> DIGITAL COMICS</a>
+             <a className="nav-link active" aria-current="page" href="#"> <img className="fotoPiccole" src={Maglietta} /> DC MERCHANDISE</a>
+             <a className="nav-link active" aria-current="page" href="#"> <img className="fotoPiccole" src={Carte} /> SUBSCRIPTION</a>
+             <a className="nav-link active" aria-current="page" href="#"> <img className="fotoPiccole" src={Gps} />COMIC SHOP LOCATOR</a>
+             <a className="nav-link active" aria-current="page" href="#"> <img className="fotoPiccole" src={Batteria} /> DC POWER VISA</a>
            </section>
           </div>
         </main>
