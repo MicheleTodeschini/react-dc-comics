3cf8d75e5a5a0893fbb2f194ea643498911dd613
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-04 14:05:19 +0100

    first commit

---
+++ src/components/main.jsx
@@ -0,0 +1,14 @@
+import Logo from "../../public/img/dc-logo.png";
+
+export default function Header(){
+    return(
+        <header>
+            <div className="container">
+                <section>
+                    <img src="Logo"></img>
+                </section>
+                <section></section>
+            </div>
+        </header>
+    )
+}
