49474205769fa8295783b78f366613a5ecbb14d1
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-04 15:52:47 +0100

    created footer and separated it in top and bottom

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,16 @@
 export default function Footer() {
     return(
         <footer>
-          
+         <div className="bg-top-footer">
+            <section className="container">
+<p>prova</p>
+            </section>
+         </div>
+         <div className="bg-bottom-footer">
+            <section className="container">
+<p>prova</p>
+            </section>
+         </div>
         </footer>
     )
 }
