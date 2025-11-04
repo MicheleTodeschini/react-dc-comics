21e856843e2050b7741e7ff5b65a6858a3dfcee1
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-04 14:19:12 +0100

    add logoin header and header, footer and main in the app.jsx file

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import Logo from "/img/dc-logo.png?url";
+
+export default function Header(){
+    return(
+        <header>
+            <div className="container">
+                <section>
+                    <img src={Logo} />
+                </section>
+                <section>
+                    prova
+                </section>
+            </div>
+        </header>
+    )
+}
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,14 +1,7 @@
-import Logo from "../../public/img/dc-logo.png";
-
-export default function Header(){
-    return(
-        <header>
-            <div className="container">
-                <section>
-                    <img src="Logo"></img>
-                </section>
-                <section></section>
-            </div>
-        </header>
+export default function Main() {
+    return (
+        <main>
+            <p>main</p>
+        </main>
     )
 }
