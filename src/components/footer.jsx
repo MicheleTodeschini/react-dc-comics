a700db41d4830420ca3f837fb584cf1b48c59db9
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-04 19:55:18 +0100

    add link and bg image in footer top

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,95 @@ export default function Footer() {
         <footer>
          <div className="bg-top-footer">
             <section className="container">
-<p>prova</p>
+                <section className="list-left">
+                    <h4>DC COMICS</h4>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Comics</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Movies</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">TV</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Games</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Videos</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">News</a>
+                    </li>
+
+                    <h4>SHOP</h4>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Shop DC</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Shop DC Collectibles</a>
+                    </li>
+                    
+                </section>
+                <section className="list-middle">
+                    <h4>DC</h4>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Terms of use</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Privacy policy (New)</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Ad choices</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Advertising</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Jobs</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Subscription</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Talent workshop</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">CPSC Certificates</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Ratings</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">Shop Help</a>
+                    </li>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">ontact Us</a>
+                    </li>
+
+                </section>
+                <section className="list-right">
+                    <h4>SITES</h4>
+                    <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">DC</a>
+                    </li>
+                      <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">MAD Magazine</a>
+                    </li>
+                      <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">DC Kids</a>
+                    </li>
+                      <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">DC Universe</a>
+                    </li>
+                      <li className="list-unstyled ">
+                        <a className="nav-link " aria-current="page" href="#">DC Power Visa</a>
+                    </li>
+                </section>
+                
             </section>
          </div>
          <div className="bg-bottom-footer">
