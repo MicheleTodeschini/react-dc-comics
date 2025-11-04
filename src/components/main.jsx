ae2fd5e68923f94139b6f0c032d958be3d19fe67
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-04 15:23:39 +0100

    add blue section

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,13 +1,18 @@
 export default function Main() {
     return (
         <main>
-            <div className="container">
-           <section className="top">
-            <h3> <i className="bi bi-arrow-right"></i>Content goes here
-            <i className="bi bi-arrow-left"></i></h3>
+            <div className="bg-black">
+           <section className="top container">
+            <h3> <i className="bi bi-arrow-right"></i>  Content goes here  
+                 <i className="bi bi-arrow-left"></i>   </h3>
            </section>
-           <section className="bottom"></section>
-           </div>
+           
+          </div>
+          <div className="bg-blue">
+           <section className="bottom container">
+            <p>prova</p>
+           </section>
+          </div>
         </main>
     )
 }
