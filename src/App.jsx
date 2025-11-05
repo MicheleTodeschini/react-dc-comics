5e96beb4389d5d649d29f78a7fae59794900ac4e
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-05 11:44:15 +0100

    add comics

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import Header from './components/header'
 import Main from "./components/main"
 import Footer from './components/footer'
 
+
+
 function App() {
 
   return (
     <>
+
+    
      <Header />
      <Main /> 
      <Footer />
